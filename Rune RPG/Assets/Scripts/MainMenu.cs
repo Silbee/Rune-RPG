31a48e5ce0396d27e7b39fab3d0a7cadbc7d4c24
Silbee <silvanderkooi@gmail.com>
2020-01-23 11:14:39 +0100

    Simple Options Menu

--- Rune RPG/Assets/Scripts/MainMenu.cs
+++ Rune RPG/Assets/Scripts/MainMenu.cs
@@ -3,9 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    Canvas menuCanvas, optionsCanvas;
+
+    private void Start()
+    {
+        menuCanvas = GameObject.Find("Main Menu").GetComponent<Canvas>();
+        optionsCanvas = GameObject.Find("Options Menu").GetComponent<Canvas>();
+    }
+
     public void StartGame() => SceneManager.LoadSceneAsync(3);
 
     public void QuitGame() => Application.Quit();
 
-    public void OpenOptions() { }
+    public void OpenOptions()
+    {
+        optionsCanvas.enabled = true;
+        menuCanvas.enabled = false;
+    }
+
+    public void CloseOptions()
+    {
+        optionsCanvas.enabled = false;
+        menuCanvas.enabled = true;
+    }
 }
