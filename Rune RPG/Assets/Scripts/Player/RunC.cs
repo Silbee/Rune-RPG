645c2f4faa4c448b29e2bbaee1f6fc6eee28d8af
Jasperbeekman <59964893+Jasperbeekman@users.noreply.github.com>
2020-02-25 12:37:43 +0100

    player run animation added to project

---
+++ Rune RPG/Assets/Scripts/Player/RunC.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RunC : MonoBehaviour
+{
+    public void Run(InputAction.CallbackContext context)
+    {
+        Vector2 wasdVector = context.ReadValue<Vector2>();
+    }
+
+}
