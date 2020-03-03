41f70e3cf349e5e918e9c26a99fa001c6b4be7b8
Jasperbeekman <59964893+Jasperbeekman@users.noreply.github.com>
2020-03-03 10:51:57 +0100

    movement prototype

--- Rune RPG/Assets/Scripts/Player/RunC.cs
+++ Rune RPG/Assets/Scripts/Player/RunC.cs
@@ -5,9 +5,18 @@ using UnityEngine.InputSystem;
 
 public class RunC : MonoBehaviour
 {
+    Vector2 moveVector;
+    public int movementSpeed = 0;
+
+
+
     public void Run(InputAction.CallbackContext context)
     {
         Vector2 wasdVector = context.ReadValue<Vector2>();
+        moveVector = wasdVector * movementSpeed;
     }
-
+    public void Update()
+    {
+        transform.Translate(moveVector.x * Time.deltaTime, moveVector.y * Time.deltaTime, 0);
+    }
 }
