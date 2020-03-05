fa3f7954e1e333da58c7a1f41208d31e160bc4c8
Jasperbeekman <59964893+Jasperbeekman@users.noreply.github.com>
2020-03-05 13:45:37 +0100

    inputShit

--- Rune RPG/Assets/Scripts/Player/RunC.cs
+++ Rune RPG/Assets/Scripts/Player/RunC.cs
@@ -8,12 +8,16 @@ public class RunC : MonoBehaviour
     Vector2 moveVector;
     public int movementSpeed = 0;
 
-
+    public Animator animator;
 
     public void Run(InputAction.CallbackContext context)
     {
         Vector2 wasdVector = context.ReadValue<Vector2>();
         moveVector = wasdVector * movementSpeed;
+
+        animator.SetFloat("Horizontal",wasdVector.x);
+        animator.SetFloat("Vertical",wasdVector.y);
+        animator.SetFloat("Magnitude",wasdVector.magnitude);
     }
     public void Update()
     {
